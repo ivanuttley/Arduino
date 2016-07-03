5f39b06254cc3b229d5276f316b2381cfc068a4d
ivanuttley <ivan.uttley@gmail.com>
2016-07-03 17:09:58 +0200

    Add files via upload

---
+++ Ivan5VLCDVoltageMeter/Ivan5VLCDVoltageMeter.ino
@@ -0,0 +1,34 @@
+
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5,4,3,2);
+int sensorValue;
+
+void setup() {
+  //Serial.begin(9600);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 1);
+  // Print a message to the LCD.
+  lcd.print("5V Voltage Meter!");
+  lcd.display();
+  delay(5000);
+}
+
+void loop() {
+  // Turn off the display:
+  lcd.noDisplay();
+  lcd.clear();
+  delay(50);
+  lcd.print(analogRead(A0));
+  lcd.display();
+  delay(500);
+  lcd.clear();
+  sensorValue = analogRead(A0);
+  lcd.print(sensorValue);
+  lcd.display();
+  delay(500);
+}
+
