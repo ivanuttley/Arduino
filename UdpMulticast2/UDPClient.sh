1a0cb735e99d3365c85205a44cd2b1b21ef15662
ivanuttley <ivan.uttley@gmail.com>
2016-07-03 17:11:32 +0200

    Add files via upload

---
+++ UdpMulticast2/UDPClient.sh
@@ -0,0 +1,2 @@
+# ivan-ubuntu works fine
+xterm -T UDPClient -e /usr/bin/cb_console_runner LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. /home/ivan/sketchbook/UDPClient/bin/Release/UDPClient 192.168.0.129 2277 
---
+++ UdpMulticast2/UDPServer.sh
@@ -0,0 +1,1 @@
+xterm -T UDPServer -e /usr/bin/cb_console_runner LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. /home/ivan/sketchbook/UDPServer/bin/Release/UDPServer 2277
